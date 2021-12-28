6f9cd15ac2d7deadb27a85b961fe5e6a8e695476
Pattaraporn <pattaraporn_ruen@cmu.ac.th>
2021-12-28 16:23:41 +0700

    add ans

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,24 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+double mean = 0,sd = 0,gm = 1,hm = 0,max = A[0],min = A[0];
+for(int i = 0;i < N;i++)
+   {
+    mean += A[i];
+    sd += pow(A[i],2);
+    gm *= A[i];
+    hm += 1/A[i];
+    if(A[i] > max)
+    max = A[i];
+    if(A[i] < min)
+    min = A[i];
+    }
+B[0] = mean/N;
+B[1] = sqrt((sd/N) - pow(B[0],2));
+B[2] = pow(gm,1.0/N);
+B[3] = N/hm;
+B[4] = max;
+B[5] = min;
+}
